a664f07e844db27c624c31f15d83f6e86692cc4c
GioVWF <giovannilizama08@gmail.com>
2023-09-15 17:37:22 -0300

    MySql connection.
    BD Commands:
    
    python manage.py makemigrations
    python manage.py migrate

--- SpaceUC/settings.py
+++ SpaceUC/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+DB_PASSWORD = "Gl28112001..."
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,8 +87,12 @@ WSGI_APPLICATION = 'SpaceUC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_spaceuc',
+        'USER': 'root',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',      
     }
 }
 
