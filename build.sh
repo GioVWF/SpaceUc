988eb08dcef06021e94098de3d05022bb103bd8b
Giovanni <giovannilizama08@gmail.com>
2025-08-29 20:37:34 -0400

    depurar el build

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Instalar dependencias
+set -e
+
+echo "--- Empezando el script de construcción ---"
+
+echo "Contenido del directorio raíz:"
+ls -la
+
+echo "Instalando dependencias de requirements.txt..."
 pip install -r requirements.txt
 
-# Ejecutar collectstatic
+echo "Ejecutando collectstatic..."
 python manage.py collectstatic --noinput
+
+echo "--- Script de construcción finalizado exitosamente ---"
