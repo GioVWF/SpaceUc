2f974ee1e34d9bb09e15d00cd7e3ce6212b9bdb6
Giovanni <giovannilizama08@gmail.com>
2025-08-29 22:11:07 -0400

    fix

--- SpaceUC/settings.py
+++ SpaceUC/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'SpaceUC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'main/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
